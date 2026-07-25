d408677d189e003aa085fe55dec3492c97e7e81f
Mo Jabid Ali <mojabidali7706864540@gmail.com>
2026-07-25 07:36:36 +0530

    removed supabse for now

--- frontend/backend/config/database.ts
+++ frontend/backend/config/database.ts
@@ -1,4 +1,4 @@
-export default ({ env }) => ({
+export default ({ env }: { env: (name: string, defaultValue?: string) => string }) => ({
   connection: {
     client: 'sqlite',
     connection: { filename: env('DATABASE_FILENAME', '.tmp/data.db') },
--- frontend/backend/config/server.ts
+++ frontend/backend/config/server.ts
@@ -1,4 +1,11 @@
-export default ({ env }) => ({
+type Env = {
+  (name: string, defaultValue?: string): string;
+  int: (name: string, defaultValue?: number) => number;
+  bool: (name: string, defaultValue?: boolean) => boolean;
+  array: (name: string, defaultValue?: string[]) => string[];
+};
+
+export default ({ env }: { env: Env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: { keys: env.array('APP_KEYS', ['key1', 'key2', 'key3', 'key4']) },
